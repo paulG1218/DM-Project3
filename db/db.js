0e6077220b08b1bc3e559942ca22d325e5ea3b3e
Jacob Foote <jacobjfoote01@gmail.com>
2023-12-05 12:33:33 -0700

    built model.js

---
+++ db/db.js
@@ -0,0 +1,24 @@
+import { Sequelize } from 'sequelize';
+
+async function connectToDB(dbURI) {
+  console.log(`Connecting to DB: ${dbURI}`);
+
+  const sequelize = new Sequelize(dbURI, {
+    logging: console.log, 
+    define: {
+      underscored: true,
+      timestamps: false,
+    },
+  });
+
+  try {
+    await sequelize.authenticate();
+    console.log('Connected to DB successfully!');
+  } catch (error) {
+    console.error('Unable to connect to DB:', error);
+  }
+
+  return sequelize;
+}
+
+export default connectToDB;
