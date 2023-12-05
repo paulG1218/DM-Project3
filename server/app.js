79187ed982560561164cda2a42b2d790095426a0
Paul Gerritsen <paul188297@gmail.com>
2023-12-05 09:24:25 -0700

    server set up

---
+++ server/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import session from "express-session";
+import ViteExpress from "vite-express";
+import morgan from "morgan";
+import handlers from "./handlers.js";
+
+const app = express();
+const port = "8000";
+ViteExpress.config({ printViteDevServerHost: true });
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(
+  session({ secret: "ssshhhhh", saveUninitialized: true, resave: false })
+);
+
+
+ViteExpress.listen(app, port, () =>
+  console.log(`Server is listening on http://localhost:${port}`)
+);
