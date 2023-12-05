4b4c38af4677a2d5f9bd229a5b9fb68999f9a17d
Paul Gerritsen <paul188297@gmail.com>
2023-12-05 10:01:05 -0700

    set up app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import './App.css'
-import { createBrowserRouter, BrowserRouter, Routes, Route } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import RegisterNewUser from './pages/RegisterNewUser'
 import Home from './pages/Home'
 import Profile from './pages/Profile'
 import Groups from './pages/Groups'
+import Root from './Root.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Root/>}>
+      //every other route goes here
+      <Route
+        index
+        element={<Home />}
+      />
+    </Route>
+  )
+)
 
 function App() {
-  return (
-    "Hello world!"
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App
