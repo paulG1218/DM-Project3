4ecee3c27a6c9bdae7634ade7a28c39d0fe81326
Jacob Foote <jacobjfoote01@gmail.com>
2023-12-07 11:52:50 -0700

    home page list view completeish

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React from "react";
+import "../css/List.css";
 
-const List = ({tasks}) => {
-
-    const taskDisplay = tasks.map(task => {
-        return (
-            <h1 key={task.taskId}>{task.title}</h1>
-        )
-    })
+const List = ({ tasks, list }) => {
+  const taskDisplay = tasks.map((task) => {
+    return (
+      <div className="taskRow">
+        <input type="checkbox" className="checkbox"></input>
+        <p className="task">{task.title}</p>
+      </div>
+    );
+  });
 
   return (
-    <div>
-        {taskDisplay}
+    <div className="list">
+      <h2>{list.listName}</h2>
+      {taskDisplay}
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;
